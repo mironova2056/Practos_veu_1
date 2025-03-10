a3178956ea24ce27f83a2ef7cdb20867fba6e855
mironova2056 <145144059+mironova2056@users.noreply.github.com>
2025-03-11 00:08:12 +0700

    big fail

--- js/main.js
+++ js/main.js
@@ -1,38 +1,43 @@
-let app = new Vue({
-    el: '#app',
-    data: {
-        product: "Socks",
-        description: "A pair of warm, fuzzy socks.",
-        brand: 'Vue Mastery',
-        altText: "A pair of socks",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks.",
-        onSale: 1,
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: "green",
-                variantImage: "./assets/vmSocks-green-onWhite.jpg",
-                variantQuantity: 10,
-            },
-            {
-                variantId: 2235,
-                variantColor: "blue",
-                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-                variantQuantity: 0,
-            }
-        ],
-        sizes: ['S', 'M', 'L', 'XL'],
-        cart: 0,
-        selectedVariant: 0,
+
+Vue.component('product', {
+    template: `<div class="product">
+    // Здесь будет весь HTML-код, который раньше был в элементе с классом product
+   </div>`,
+    data() {
+        return {
+            product: "Socks",
+            description: "A pair of warm, fuzzy socks.",
+            brand: 'Vue Mastery',
+            altText: "A pair of socks",
+            link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks.",
+            onSale: 1,
+            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: "green",
+                    variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                    variantQuantity: 10,
+                },
+                {
+                    variantId: 2235,
+                    variantColor: "blue",
+                    variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                    variantQuantity: 0,
+                }
+            ],
+            sizes: ['S', 'M', 'L', 'XL'],
+            cart: 0,
+            selectedVariant: 0,
+        }
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
         updateProduct(index) {
-          this.selectedVariant = index;
-          console.log(index);
+            this.selectedVariant = index;
+            console.log(index);
         },
         deleteFromCart() {
             if (this.cart > 0) {
@@ -45,7 +50,7 @@ let app = new Vue({
             return this.brand + ' ' + this.product;
         },
         image(){
-          return this.variants[this.selectedVariant].variantImage;
+            return this.variants[this.selectedVariant].variantImage;
         },
         inStock() {
             return this.variants[this.selectedVariant].variantQuantity;
@@ -57,5 +62,10 @@ let app = new Vue({
                 return this.brand + ' ' + this.product + 'NOT On Sale';
             }
         }
-    },
+    }
+})
+
+
+let app = new Vue({
+    el: '#app',
 });
