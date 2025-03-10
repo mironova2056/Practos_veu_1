6bbbe7a251eace8e473709a784e992d3d4f3de86
mironova2056 <145144059+mironova2056@users.noreply.github.com>
2025-03-10 23:44:22 +0700

    calculated properties

--- js/main.js
+++ js/main.js
@@ -3,38 +3,59 @@ let app = new Vue({
     data: {
         product: "Socks",
         description: "A pair of warm, fuzzy socks.",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
+        brand: 'Vue Mastery',
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks.",
-        inStock: false,
-        onSale: true,
+        onSale: false,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: "green",
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10,
             },
             {
                 variantId: 2235,
                 variantColor: "blue",
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0,
             }
         ],
         sizes: ['S', 'M', 'L', 'XL'],
         cart: 0,
+        selectedVariant: 0,
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-          this.image = variantImage
+        updateProduct(index) {
+          this.selectedVariant = index;
+          console.log(index);
         },
         deleteFromCart() {
             if (this.cart > 0) {
                 this.cart -= 1
             }
         },
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image(){
+          return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
+        },
+        sale(){
+            if(this.onSale){
+                return this.brand + ' ' + this.product + 'On Sale';
+            } else {
+                return this.brand + ' ' + this.product + 'NOT On Sale';
+            }
+        }
     },
 });
